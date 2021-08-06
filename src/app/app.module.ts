74afe02ab693df6be48d83b0440fc882ed6d8213
Tuyen Pham <tuh39709@temple.edu>
2021-08-06 16:11:45 -0400

    add item form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { AddItemFormComponent } from './add-item-form/add-item-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    AddItemFormComponent
   ],
   imports: [
     BrowserModule,
